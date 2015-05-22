faf22c794beba74b7b7e5a11659118c145fc26a7
Michael van Huis <michaelvanhuis@hotmail.com>
2015-05-22 15:44:44 +0200

    Counter voor drives, taal verandering

--- m_drivedetect.py
+++ m_drivedetect.py
@@ -7,13 +7,14 @@ options = int(input("1: Show drive info" + "\n"))
 
 def driveinfolinux():
 #Cross platform schijf info
-    diskspartities = str(psutil.disk_partitions())
-    diskssplit = diskspartities.split('),')
+    diskinfo = str(psutil.disk_partitions())
+    diskinfolist = diskinfo.split('),')
+    x = 0
+#Split voor aantal aanwezige schijven
+    for num in diskinfolist:
+        x = x+1
+        print ("Drive",x, ":" + num)
 
-    for num in diskssplit:
-        print (num)
-
+#Call function
 if options == 1:
     driveinfolinux()
-
-
