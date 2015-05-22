4aca7bbbe2157811588bbcc7fa28dde43483cd6b
michaelvanhuis <Mvanhuis@hotmail.com>
2015-05-22 20:25:50 +0200

    User and system information

--- usernames.py
+++ usernames.py
@@ -5,7 +5,10 @@ import socket
 
 def userinfo():
 #User info
-    print ("Hostname:", socket.gethostname())
-    print ("Username:", getpass.getuser())
+    print ("Network name:           ",  socket.gethostname())
+    print ("Username:               ",  getpass.getuser())
+    print ("System architecture:    ",  platform.machine())
+    print ("Operating system        ",  platform.platform(aliased=0, terse=0))
+    print ("Processor:              ",  platform.processor())
 
 userinfo()
