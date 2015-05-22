9df98d531c6a57091613dd1796209acbbe985ce8
Bart <l0ngestever@github>
2015-05-22 16:08:23 +0200

    add some new features

--- pythronic.py
+++ pythronic.py
@@ -13,17 +13,22 @@ import os
 
 opeSys = False
 opeSysSlash = False
+user = False
 
 
 def main():
     detectOs()
     printWelcomeScreen()
     startApplication()
+    login()
+    print ("succesvol ingelogd! : ", user)
 
 
 def startApplication():
     if not os.path.isfile("./db/pythronic.db"):
+        print (" [ERROR]: Database doesn't exist; executing setup.\n")
         setup.createDatabase()
+        printWelcomeScreen()
 
 
 def detectOs():
@@ -45,13 +50,8 @@ def detectOs():
 
 def printWelcomeScreen():
     clearScreen()
-
-    # message = '######################\n\n'
-    # message += ' Welcome by Pytronic! \n\n'
-    # message += '######################\n'
-
-    message = '\n Welcome by Pytronic! \n\n'
-
+    message = '\n'
+    message += ' Welcome by Pytronic! \n\n'
     return print (message)
 
 
@@ -63,5 +63,27 @@ def clearScreen():
     elif opeSys == "windows":
         os.system("cls")
 
+
+def login():
+    print (' Login first:\n')
+    username = input(" Username: ")
+    password = input(" Password: ")
+
+    while (checkLogin(username, password)):
+        print ('\n Login failed, try again: \n')
+        username = input(" Username: ")
+        password = input(" Password: ")
+        checkLogin(username, password)
+
+    return True
+
+
+def checkLogin(username, password):
+    if username == 'bart' and password == 'bart':
+        global user
+        user = username
+        return False
+    else:
+        return True
 
 main()
--- setup.py
+++ setup.py
@@ -10,6 +10,32 @@
     SETUP ONLY SCRIPT
 """
 import sqlite3
+import hashlib
 
 def createDatabase():
-    return print ('Database wordt gemaakt...')
+    db = sqlite3.connect("db/pythronic.db")
+
+    createDBTables(db)
+    createDBUser(db)
+
+    db.close()
+
+
+def createDBTables(db):
+    cursor = db.cursor()
+    sql = ('''CREATE TABLE users (id INTEGER PRIMARY KEY,
+              name TEXT, pass TEXT, deleted INTEGER);
+              CREATE TABLE logs (id INTEGER PRIMARY KEY,
+              datetime TIMESTAMP, description TEXT)''')
+    db.executescript(sql)
+    print (" [INFO]: Tables `users` and `logs` created succesfully.\n")
+
+
+def createDBUser(db):
+    username = input(" Username: ")
+    password = input(" Password: ")
+
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO users (name, pass, deleted) VALUES (?,?,?)''',
+                      (username, hashlib.md5(password.encode('utf-8')).hexdigest(), '0'))
+    db.commit()
