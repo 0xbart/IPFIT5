97282cf50b4b56f19429a6acac83e3baf1a1e875
michaelvanhuis <Mvanhuis@hotmail.com>
2015-05-26 10:59:08 +0200

    Windows image finder and exif reader

---
+++ exifreadwin.py
@@ -0,0 +1,17 @@
+__author__ = 'Michael'
+import exifread
+import os
+
+disk = input("Type a drive letter to search for exif data:" + "\n")
+path = disk + ":\\"
+
+for root, subdirs, files in os.walk(path):
+    for file in files:
+        if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg', '.tif', '.tiff'):
+            path_name = (os.path.join(root, file))
+            f = open(path_name, 'rb')
+            tags = exifread.process_file(f)
+            if not tags:
+                print(path_name, "(No exif data)")
+            else:
+                print(path_name, tags)
