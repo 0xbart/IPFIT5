a70a373d080e363d9b43629377e8fe887e1176c5
Chester <chestervdb@live.nl>
2015-06-16 12:37:34 +0200

    Mousejiggler v1.0

--- mousejiggler.py
+++ mousejiggler.py
@@ -1,11 +1,38 @@
+#Source http://pyautogui.readthedocs.org/en/latest/mouse.html
+#Quartz installeren: pyobjc-framework-Quartz
+#Versie 1.0
+
 #Auteur
 __author__ = 'Chester'
 
 #Importeren van modules
-from pymouse import PyMouse
+import pyautogui, sys
 
-#Aanmaken van instance
-m = PyMouse()
+while True:
+    pxl = raw_input("Vul het aantal pixels in dat de muis moet bewegen: ")
+    try:
+        val = int(pxl)
+        print("Het programma is gestart. Druk op crtl+c om de mousejiggler te beeindigen.")
+        break
+    except ValueError:
+        print("Er is een ander karakter dan een getal ingevuld. Probeer het opnieuw.")
 
-#Bewegen van muis
-m.move(200, 200)
+
+try:
+    count = 0
+    while True:
+        x, y = pyautogui.position()
+        if count % 2 == 1:
+            pyautogui.moveTo(int(x)+int(pxl), None)
+        else:
+            pyautogui.moveTo(int(x)-int(pxl), None)
+        count = count + 1
+except KeyboardInterrupt:
+    print '\n'
+
+print ("Het programma is beeindigd.")
+
+
+
+#to-do
+#escape chars inbouwen
