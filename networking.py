5370ac5d8ddd9f3c19e21117def491c4623769fa
Wesley Boumans <w.boumans89@gmail.com>
2015-06-16 14:20:16 +0200

    Networking updated, try and catch is added in a later stage

--- networking.py
+++ networking.py
@@ -13,37 +13,39 @@ def determine():
         print "Unix detected!"
         operationSoftware = "Unix" #Mac
 
-#Defining network commando's different operation systems
+#Set up network command
 def network():
     global operationSoftware
     if operationSoftware == "Windows":
-        with open('unsortedNetworkDetails.txt', "w") as outfile:
+        with open('networking.txt', "w") as outfile:
             subprocess.call('ipconfig.exe /all', stdout=outfile)
     elif operationSoftware == "Unix":
-        with open('unsortedNetworkDetails.txt', "w") as outfile:
+        with open('networking.txt', "w") as outfile:
             subprocess.call("ifconfig", stdout=outfile)
 
 
 def sortingProc():
     global operationSoftware
-    unixKeywords = ["inet"] #Keywords waarbij binnen Linux gefilterd wordt
-    windowsKeywords = ["IP", "DHCP"] #Keywords waar binnen Windows gefilterd wordt
+    unixKeywords = ["inet", "IP"]
+    windowsKeywords = ["IP", "DHCP"]
+
     temp = []
 
     if operationSoftware == "Windows":
         print("Windows sorting")
-        with open("unsortedNetworkDetails.txt", "r") as file_to_read:
+        with open("networking.txt", "r") as file_to_read:
             for line in file_to_read:
-                if str(windowsKeywords) in line:
-                    temp.append(line + ",")
+                for i, j in enumerate(windowsKeywords):
+                    if j in line:
+                        temp.append(line + ",")
                     with open("output.txt", "w") as op:
                         op.writelines((items) for items in temp)
-            print("Output file has been created, check output.txt")
     elif operationSoftware == "Unix":
-        with open("unsortedNetworkDetails.txt", "r") as file_to_read:
+        with open("networking.txt", "r") as file_to_read:
             for line in file_to_read:
-                if str(unixKeywords) in line:
-                    temp.append(line)
+                for i, j in enumerate(unixKeywords):
+                    if j in line:
+                        temp.append(line)
                     with open("output.txt", "w") as op:
                         op.writelines((items) + "," for items in temp)
             print("Output file has been created, check output.txt")
