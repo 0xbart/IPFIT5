d2fb166da78794aa88683301141c43e0f25181b9
Michael van Huis <michaelvanhuis@hotmail.com>
2015-06-29 13:14:41 +0200

    try-except updated

--- systeminfo.py
+++ systeminfo.py
@@ -6,22 +6,26 @@ import platform
 from psutil import virtual_memory
 
 def userinfo():
-    mem = virtual_memory()
-    c = wmi.WMI()
-    for i in c.Win32_Processor ():
-        cputype = i.Name
+    try:
+        mem = virtual_memory()
+        c = wmi.WMI()
+        for i in c.Win32_Processor ():
+            cputype = i.Name
 
-    hardwarelist = ["PC name:                            " + socket.gethostname(),
-                    "Username:                           " +  getpass.getuser() ,
-                    "System architecture:                " +  platform.machine(),
-                    "Operating system:                   " +  platform.platform(aliased=0, terse=0),
-                    "Processor name:                     " +  cputype,
-                    "Processor family:                   " +  platform.processor(),
-                    "Total virtual memory in Gb:         " + str((mem.total/(math.pow(2,30)))),
-                    "Used virtual memory in Gb           " + str((mem.used/(math.pow(2,30)))),
-                    "Available virtual memory in Gb:     " + str((mem.available/(math.pow(2,30))))
-                    ]
-    #Get RAM and CPU name
+        hardwarelist = ["PC name:                            " + socket.gethostname(),
+                        "Username:                           " +  getpass.getuser() ,
+                        "System architecture:                " +  platform.machine(),
+                        "Operating system:                   " +  platform.platform(aliased=0, terse=0),
+                        "Processor name:                     " +  cputype,
+                        "Processor family:                   " +  platform.processor(),
+                        "Total virtual memory in Gb:         " + str((mem.total/(math.pow(2,30)))),
+                        "Used virtual memory in Gb           " + str((mem.used/(math.pow(2,30)))),
+                        "Available virtual memory in Gb:     " + str((mem.available/(math.pow(2,30))))
+                        ]
+    except:
+        ImportError
+
+        #Get RAM and CPU name
     try:
         for i in hardwarelist:
             print i
@@ -29,3 +33,4 @@ def userinfo():
         pass
 
 userinfo()
+
