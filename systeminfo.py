da0e434d223298b2a703da86d28fddefc9974f70
Michael van Huis <michaelvanhuis@hotmail.com>
2015-06-29 13:02:44 +0200

    try-except added

--- systeminfo.py
+++ systeminfo.py
@@ -6,24 +6,26 @@ import platform
 from psutil import virtual_memory
 
 def userinfo():
+    mem = virtual_memory()
+    c = wmi.WMI()
+    for i in c.Win32_Processor ():
+        cputype = i.Name
+
+    hardwarelist = ["PC name:                            " + socket.gethostname(),
+                    "Username:                           " +  getpass.getuser() ,
+                    "System architecture:                " +  platform.machine(),
+                    "Operating system:                   " +  platform.platform(aliased=0, terse=0),
+                    "Processor name:                     " +  cputype,
+                    "Processor family:                   " +  platform.processor(),
+                    "Total virtual memory in Gb:         " + str((mem.total/(math.pow(2,30)))),
+                    "Used virtual memory in Gb           " + str((mem.used/(math.pow(2,30)))),
+                    "Available virtual memory in Gb:     " + str((mem.available/(math.pow(2,30))))
+                    ]
     #Get RAM and CPU name
     try:
-        mem = virtual_memory()
-        c = wmi.WMI()
-        for i in c.Win32_Processor ():
-            cputype = i.Name
-
-        print("PC name:                            ",  socket.gethostname())
-        print("Username:                           ",  getpass.getuser())
-        print("System architecture:                ",  platform.machine())
-        print("Operating system:                   ",  platform.platform(aliased=0, terse=0))
-        print("Processor name:                     ",  cputype)
-        print("Processor family:                   ",  platform.processor())
-        print("Total virtual memory in Gb:         ", (mem.total/(math.pow(2,30))))
-        print("Used virtual memory in Gb           ", (mem.used/(math.pow(2,30))))
-        print("Available virtual memory in Gb:     ", (mem.available/(math.pow(2,30))))
-
+        for i in hardwarelist:
+            print i
     except:
-        print "Failure to detect system hardware"
+        pass
 
 userinfo()
