a19a54284e2e5ade70bf185e0bfdd6ee309f80de
Michael van Huis <michaelvanhuis@hotmail.com>
2015-06-29 12:10:34 +0200

    try-except added

--- systeminfo.py
+++ systeminfo.py
@@ -1,18 +1,29 @@
-__author__ = 'Michael'
-
 import getpass
 import socket
-import sysinfo
+import wmi
+import math
 import platform
-import psutil
+from psutil import virtual_memory
 
 def userinfo():
-#User info
-    print ("Network name:           ",  socket.gethostname())
-    print ("Username:               ",  getpass.getuser())
-    print ("System architecture:    ",  platform.machine())
-    print ("Operating system        ",  platform.platform(aliased=0, terse=0))
-    print ("Processor:              ",  platform.processor())
-    print ("Virtual memory:         ",  psutil.virtual_memory())
-    print ("Virtual memory:         ",  sysinfo.memory_available())
+    #Get RAM and CPU name
+    try:
+        mem = virtual_memory()
+        c = wmi.WMI()
+        for i in c.Win32_Processor ():
+            cputype = i.Name
+
+        print("PC name:                            ",  socket.gethostname())
+        print("Username:                           ",  getpass.getuser())
+        print("System architecture:                ",  platform.machine())
+        print("Operating system:                   ",  platform.platform(aliased=0, terse=0))
+        print("Processor name:                     ",  cputype)
+        print("Processor family:                   ",  platform.processor())
+        print("Total virtual memory in Gb:         ", (mem.total/(math.pow(2,30))))
+        print("Used virtual memory in Gb           ", (mem.used/(math.pow(2,30))))
+        print("Available virtual memory in Gb:     ", (mem.available/(math.pow(2,30))))
+
+    except:
+        print "Failure to detect system hardware"
+
 userinfo()
