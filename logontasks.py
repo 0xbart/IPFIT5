10529ddf55245d5e8bb19007410aef71d5713f39
Michael van Huis <michaelvanhuis@hotmail.com>
2015-06-29 11:40:08 +0200

    final version for windows

--- logontasks.py
+++ logontasks.py
@@ -1,6 +1,4 @@
-__author__ = 'Michael'
-
-from winreg import *
+from _winreg import *
 
 print (r"*** Reading from registry SOFTWARE\Microsoft\Windows\CurrentVersion\Run ***")
 aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
@@ -8,11 +6,9 @@ aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run")
 for i in range(1024):
     try:
         n,v,t = EnumValue(aKey,i)
-        print (i, n, v, t)
+        print (i+1, n, v, t)
     except EnvironmentError:
-        print ("You have",i,"tasks starting at logon.")
+        print ("You have",i,"tasks starting at logon, that are recorded in the registry.")
         break
 
 CloseKey(aKey)
-
-
