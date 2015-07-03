6ce0fac2980ab9dd518f6400958d5d5a3479b163
michaelvanhuis <michaelvanhuis@hotmail.com>
2015-07-03 13:41:59 +0200

    different function

--- hardwarelinux.py
+++ hardwarelinux.py
@@ -1,7 +1,15 @@
 import os
-
-__author__ = 'Michael'
+import platform
 
 # List hardware on Linux using a command
+try:
+	print "OS:",  platform.uname()[0]
+	print "Distro:", platform.linux_distribution()
+	print "Architecture:", platform.architecture()
+	print "Processors:" 
+	print os.system("grep 'model name' /proc/cpuinfo")
+	print "RAM: "
+	print os.system("cat /proc/meminfo | grep MemTotal | awk '{ print $2 }'")
 
-os.system("hwinfo")
+except:
+	print ("Could not detect hardware")
