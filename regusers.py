97ee56f1bc70b6d54baeaadb64292a6976e0f775
michaelvanhuis <michaelvanhuis@hotmail.com>
2015-07-06 19:23:28 +0200

    semi-functional user profile finder

--- regusers.py
+++ regusers.py
@@ -1,15 +1,41 @@
 from _winreg import *
 
-print (r"*** Reading from SOFTWARE\Microsoft\WindowsNT\CurrentVersion\ProfileList ***")
-aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+# Make list for folder paths
 
-aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList")
-for i in range(1024):
-    try:
-        print (QueryInfoKey(aReg))
-        n,v,t = EnumValue(aKey,i)
-        print (i, n, v)
-    except EnvironmentError:
-        print ("You have", i, "tasks starting at logon...")
-        break
-CloseKey(aKey)
+keylist = []
+
+# Loop through registry for all folders containing user profiles
+
+
+def userlisting():
+    ProfileList = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+    regcontent = OpenKey(ProfileList, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList")
+
+# Scan for up to a maximum of 256 folders
+
+    for i in range(256):
+        try:
+            keyname = EnumKey(regcontent, i)
+            keylist.append("r" + "\"" "SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+                           + "\\" + str(keyname) + "\"")
+        except WindowsError:
+            pass
+userlisting()
+
+print ("*** Reading from SOFTWARE\Microsoft\WindowsNT\CurrentVersion\ProfileList ***")
+folderkeys = keylist
+
+for i in folderkeys:
+    print i
+
+kiesregister = str(input("Copy and paste of the specified paths completely, to read profile data:" + "\n"))
+
+aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+t = OpenKey(aReg, kiesregister)
+# list all values for a key
+try:
+    count = 0
+    name, value, type = EnumValue(t, count)
+    print (value),
+except WindowsError:
+    pass
