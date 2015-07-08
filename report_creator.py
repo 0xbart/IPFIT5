15d5f824993a97aa6a10ae8c26ea0343f4ab9072
l0ngestever <bartmau93@hotmail.com>
2015-07-08 22:36:41 +0200

    Update report

--- report_creator.py
+++ report_creator.py
@@ -1,89 +1,106 @@
 import sqlite3
 
-# Creates or opens a file called mydb with a SQLite3 DB
-#db = sqlite3.connect(getCaseDatabase(casename))
-db = sqlite3.connect('testingaaa.db') #### NOG AANPASSEN, OOK DE SQL STATEMENTS
+db = sqlite3.connect('db/cases/Bart.db')
 cursor = db.cursor()
+
+cursor.execute('''SELECT id, name, description, type, created_at, deleted FROM evidences''')
+fetchEvidences = cursor.fetchall()
+
+cursor.execute('''SELECT id, name, description, created_at FROM general''')
+fetchGeneral = cursor.fetchall()
 
 html = ''
 html += '''
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
-<title>Pythronic - Report</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-<link rel="stylesheet" type="text/css" href="bootstrap/css/font-awesome.min.css" />
-<script type="text/javascript" src="bootstrap/js/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <meta charset="utf-8" />
+    <title>Pythronic - Report</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/font-awesome.min.css" />
+    <script type="text/javascript" src="bootstrap/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
-<div class="page-header">
-<h1>Pythronic <small>Report</small></h1>
-</div>
-<div class="container">
-<div class="alert alert-warning alert-dismissible" role="alert">
-<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-Dit is het automatisch gerenegeerde testrapport. Bekijk de resultaten in de uitklapbare lijsten.
-</div>
-<div class="panel-group" id="accordion">
-<div class="faqHeader">Case</div>
-<div class="panel panel-default">
-<div class="panel-heading">
-<h4 class="panel-title">
+    <div class="container">
+    <div class="page-header">
+        <h1>Pythronic <small>Report</small></h1>
+    </div>
+    <div class="container">
+    <div class="alert alert-warning alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        Dit is het automatisch gerenegeerde testrapport. Bekijk de resultaten in de uitklapbare lijsten.
+    </div>
+    <div class="panel-group" id="accordion">
+    <div class="faqHeader">Case</div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">
+        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> Bewijsmateriaal</a>
+        </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse">
+    <div class="panel-body">
+        <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>Naam</th>
+            <th>Beschrijving</th>
+            <th>Type</th>
+            <th>Aangemaakt op:</th>
+            <th>Verwijderd</th>
+        </tr>
+'''
 
-<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> \
-Bewijsmateriaal</a>
-</h4></div>
-<div id="collapseOne" class="panel-collapse collapse">
-<div class="panel-body">'''
+for row in fetchEvidences:
+    html += ('<tr>')
+    html += ('<td>{0}</td><td>{1}</td><td>{2}<td>{3}<td>{4}<td>{5}'
+              .format(row[0],
+                      row[1],
+                      row[2] if row[2] != '' else '-',
+                      row[3],
+                      row[4],
+                      'Ja' if row[5] == '1' else 'Nee') + '</td>')
+    html += ('</tr>')
+
 html += '''
-<table class="table">
-<tr><th>ID</th><th>Naam</th><th>Beschrijving</th><th>Type</th><th>Aangemaakt op:</th><th>Verwijderd</th></tr>'''
+        </table>
+    </div>
+    </div>
+    </div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">
+        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Algemeen</a>
+        </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+    <div class="panel-body">
+        <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>Naam</th>
+            <th>Beschrijving</th>
+            <th>Aangemaakt op</th>
+            </tr>
+'''
 
-cursor.execute('''SELECT id, name, description, type, created_at, deleted FROM evidences''')
-table = cursor.fetchall() #retrieve the first row
-
-for row in table:
+for row in fetchGeneral:
      html += ('<tr>')
-     html += ('<td>' '{0}''</td> ' '<td>' '{1}' '</td> ' '<td>' '{2}' '<td>' '{3}''<td>' '{4}''<td>' '{5}'.format(row[0], row[1], row[2], row[3], row[4], row[5]) + '</td>')
+     html += ('<td>{0}</td><td>{1}</td><td>{2}<td>{3}'
+              .format(row[0],
+                      row[1],
+                      row[2] if row[2] != '' else '-',
+                      row[3]) + '</td>')
      html += ('</tr>')
 
 
 html += '''
-</table>
-</div>
-</div>
-</div>
-<div class="panel panel-default">
-<div class="panel-heading">
-<h4 class="panel-title">
-
-<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"> \
-Algemeen</a>
-</h4></div>
-<div id="collapseTwo" class="panel-collapse collapse">
-<div class="panel-body">'''
-html += '''
-<table class="table">
-<tr><th>ID</th><th>Naam</th><th>Beschrijving</th><th>Aangemaakt op:</th></tr>'''
-
-cursor.execute('''SELECT id, name, description, created_at FROM general''')
-table = cursor.fetchall() #retrieve the first row
-
-for row in table:
-     html += ('<tr>')
-     html += ('<td>' '{0}''</td> ' '<td>' '{1}' '</td> ' '<td>' '{2}' '<td>' '{3}'.format(row[0], row[1], row[2], row[3]) + '</td>')
-     html += ('</tr>')
-
-
-html += '''
-</table>
-</div>
-</div>
-</div>
+        </table>
+    </div>
+    </div>
+    </div>
 <div class="faqHeader">Modules</div>
 <div class="panel panel-default">
 <div class="panel-heading">
@@ -93,12 +110,11 @@ html += '''
 Hardware info</a>
 </h4></div>
 <div id="collapseThree" class="panel-collapse collapse">
-<div class="panel-body">'''
-html += '''
+<div class="panel-body">
 <table class="table">
 <tr><th>ID</th><th>CPU</th><th>USB Devices</th><th>Architectuur</th><th>CPU Naam</th><th>CPU Family</th><th>Gebruikte Geheugen</th><th>Vrije Geheugen</th><th>Totaal Geheugen</th></tr>'''
 
-cursor.execute('''SELECT id, processor, usb_devices, system_arch, proc_name, proc_family, used_memory, free_memory, total_memory FROM laptop_hardware''')
+cursor.execute('''SELECT id, processor, usb_devices, system_arch, proc_name, proc_family, used_memory, free_memory, total_memory FROM PC_hardware''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -126,7 +142,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Naam</th></tr>'''
 
-cursor.execute('''SELECT id, name FROM laptop_software''')
+cursor.execute('''SELECT id, name FROM PC_software''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -154,7 +170,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Dropbox</th><th>OneDrive</th><th>Evernote</th><th>Google Drive</th></tr>'''
 
-cursor.execute('''SELECT id, dropbox, onedrive, evernote, googledrive FROM laptop_cloud''')
+cursor.execute('''SELECT id, dropbox, onedrive, evernote, googledrive FROM PC_cloud''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -182,7 +198,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Chrome</th><th>FireFox</th><th>Internet Explorer</th></tr>'''
 
-cursor.execute('''SELECT id, his_chrome, his_ff, his_iexplorer FROM laptop_browser''')
+cursor.execute('''SELECT id, his_chrome, his_ff, his_iexplorer FROM PC_browser''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -208,9 +224,9 @@ Detected drives</a>
 <div class="panel-body">'''
 html += '''
 <table class="table">
-<tr><th>ID</th><th>Drive Naam:</th><th>Drive Detail:</th></tr>'''
+<tr><th>ID</th><th>Drive Naam:</th><th>Drive Mount:</th><th>Drive filesystem:</th></tr>'''
 
-cursor.execute('''SELECT id, drive_name, drive_detail FROM laptop_drive''')
+cursor.execute('''SELECT id, drive_name, drive_mountpoint, drive_filesystem FROM PC_drive''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -238,7 +254,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Bestandsnaam:</th><th>Parent:</th><th>SHA-hash:</th><th>MD5-hash:</th></tr>'''
 
-cursor.execute('''SELECT id, name, parent, shahash, md5hash FROM laptop_files''')
+cursor.execute('''SELECT id, name, size, shahash, md5hash FROM PC_files''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -266,7 +282,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Naam</th></tr>'''
 
-cursor.execute('''SELECT id, name FROM laptop_linux_logon''')
+cursor.execute('''SELECT id, name FROM PC_linux_logon''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -293,7 +309,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Naam</th></tr>'''
 
-cursor.execute('''SELECT id, name FROM laptop_users''')
+cursor.execute('''SELECT id, name FROM PC_users''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -320,7 +336,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Naam</th></tr>'''
 
-cursor.execute('''SELECT id, name FROM laptop_win_logon''')
+cursor.execute('''SELECT id, name FROM PC_win_logon''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -347,7 +363,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>Virus naam</th><th>Virus hash</th><th>Details</th></tr>'''
 
-cursor.execute('''SELECT id, virus_name, virus_hash, virus_output FROM laptop_virus''')
+cursor.execute('''SELECT id, virus_name, virus_hash, virus_output FROM PC_virus''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -374,7 +390,7 @@ html += '''
 <table class="table">
 <tr><th>ID</th><th>IP</th><th>MAC</th><th>IP Connected:</th></tr>'''
 
-cursor.execute('''SELECT id, ip, mac, connected_ip FROM laptop_network''')
+cursor.execute('''SELECT id, ip, mac, connected_ip FROM PC_network''')
 table = cursor.fetchall() #retrieve the first row
 
 for row in table:
@@ -425,6 +441,6 @@ html += '</body>'
 html += '</html>'
 
 
-file = open('test.html', 'w')
+file = open('report.html', 'w')
 file.write(html)
 file.close()
